e2c6a1569e5bccce736f21a96ade472a0169d053
Rodrigo <2rodrigosilva2@gmail.com>
2025-12-17 21:24:22 -0300

    feat:aulas oop2

--- Aula03/Usuario.js
+++ Aula03/Usuario.js
@@ -1,25 +1,109 @@
 export default class Usuario {
+    #nome;
+    #idade;
+    #genero;
+    #telefone;
+    #email;
+    #cpf;
+    #cep;
+    #rg;
+    #endereco;
+
     constructor(nome, idade, genero, telefone, email, cpf, cep, rg, endereco) {
-        this.nome = nome;
-        this.idade = idade;
-        this.genero = genero;
-        this.telefone = telefone;
-        this.email = email;
-        this.cpf = cpf;
-        this.cep = cep;
-        this.rg = rg;
-        this.endereco = endereco;
+        this.#nome = nome;
+        this.#idade = idade;
+        this.#genero = genero;
+        this.#telefone = telefone;
+        this.#email = email;
+        this.#cpf = cpf;
+        this.#cep = cep;
+        this.#rg = rg;
+        this.#endereco = endereco;
+
+        Object.freeze(this);
+    }
+
+    get getNome() {
+        return this.#nome;
+    }
+
+    setNome(nome) {
+        this.#nome = nome;
+    }
+
+    get getIdade() {
+        return this.#idade;
+    }
+
+    setIdade(idade) {
+        this.#idade = idade;
+    }
+
+    get getGenero() {
+        return this.#genero;
+    }
+
+    setGenero(genero) {
+        this.#genero = genero;
+    }
+
+    get getTelefone() {
+        return this.#telefone;
+    }
+
+    setTelefone(telefone) {
+        this.#telefone = telefone;
+    }
+
+    get getEmail() {
+        return this.#email;
+    }
+
+    setEmail(email) {
+        this.#email = email;
+    }
+
+    get getCpf() {
+        return this.#cpf;
+    }
+
+    setCpf(cpf) {
+        this.#cpf = cpf;
+    }
+
+    get getCep() {
+        return this.#cep;
+    }
+
+    setCep(cep) {
+        this.#cep = cep;
+    }
+
+    get getRg() {
+        return this.#rg;
+    }
+
+    setRg(rg) {
+        this.#rg = rg;
+    }
+
+    get getEndereco() {
+        return this.#endereco;
+    }
+
+    setEndereco(endereco) {
+        this.#endereco = endereco;
     }
 
     mostrarDados() {
-        return `Nome: ${this.nome}
-        Idade: ${this.idade} anos
-        Genero: ${this.genero}
-        Telefone: ${this.telefone}
-        Email: ${this.email}
-        CPF: ${this.cpf}
-        CEP: ${this.cep}
-        RG: ${this.rg}
-        Endereço: ${this.endereco}`
+        return `Nome: ${this.#nome}
+        Idade: ${this.#idade} anos
+        Genero: ${this.#genero}
+        Telefone: ${this.#telefone}
+        Email: ${this.#email}
+        CPF: ${this.#cpf}
+        CEP: ${this.#cep}
+        RG: ${this.#rg}
+        Endereço: ${this.#endereco}`
     }
 }
--- Aula03/index.js
+++ Aula03/index.js
@@ -2,4 +2,14 @@ import Usuario from "../Aula03/Usuario.js";
 
 let pessoa01 = new Usuario("Rodrigo", 22, "masculino", "(84)4002-8922", "cremedegalinha@email.com", "123.321.213.13", "123456", "654321", "Rua de um lugar legal")
 
-console.log(pessoa01.mostrarDados());
+console.log(pessoa01.getNome)
+
+pessoa01.setNome("Vini")
+
+console.log(pessoa01.getNome)
+
+console.log(pessoa01.getCpf)
+
+pessoa01.setCpf("321.321.321.12")
+
+console.log(pessoa01.getCpf)
