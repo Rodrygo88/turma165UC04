e02efe8664a76d6b6e2ee887416149b39acdf875
Rodrigo Silva <89754374+Rodrygo88@users.noreply.github.com>
2026-03-11 21:17:40 -0300

    add

---
+++ UC03/aula02/back/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import filmeRoutes from "./src/routes/filmeRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(
+    cors(
+        {origin: process.env.API_URL_FRONT}
+    )
+)
+
+app.use("/filmes", filmeRoutes);
+
+app.get("/", (req, res)=>{
+    res.status(200).json({msg: "Hello World!"});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+});
