66d0907616a06d87d344f0dd5491e87d0c436e3d
Rodrigo Silva <89754374+Rodrygo88@users.noreply.github.com>
2026-03-12 21:17:14 -0300

    Update db.js

--- UC03/aula02/back/src/config/db.js
+++ UC03/aula02/back/src/config/db.js
@@ -5,7 +5,7 @@ const {Pool} = pkg;
 const db = new Pool(
     {
         host: process.env.DB_HOST,
-        user: process.env.DB_USE,
+        user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         port: process.env.DB_PORT
