bf7767a2f959cf0ce87497ae0ab144bd789737d1
Rodrigo Silva <89754374+Rodrygo88@users.noreply.github.com>
2026-03-19 21:20:50 -0300

    add

--- UC03/aula02/front/src/pages/user/FilmesUsuarioPage.jsx
+++ UC03/aula02/front/src/pages/user/FilmesUsuarioPage.jsx
@@ -6,6 +6,8 @@ import "./FilmesUsuarioPage.css";
 export default function FilmesUsuarioPage() {
   const [filmes, setFilmes] = useState([]);
   const [busca, setBusca] = useState("");
+  const [ordenacao, setOrdenacao] = useState("");
+
   async function carregarFilmes() {
     try {
       const response = await listarFilmes();
@@ -22,8 +24,45 @@ export default function FilmesUsuarioPage() {
     filme.titulo.toLowerCase().includes(busca.toLowerCase()),
   );
 
+  const filmesOrdenados = [...filmesFiltrados].sort((a, b) => {
+    if (ordenacao === "titulo-asc") {
+      return a.titulo.localeCompare(b.titulo);
+    }
+    if (ordenacao === "titulo-desc") {
+      return b.titulo.localeCompare(a.titulo);
+    }
+
+    if (ordenacao === "genero-asc") {
+      return a.genero.localeCompare(b.genero);
+    }
+    if (ordenacao === "genero-desc") {
+      return b.genero.localeCompare(a.genero);
+    }
+
+    if (ordenacao === "ano-asc") {
+      return a.ano - b.ano;
+    }
+    if (ordenacao === "ano-desc") {
+      return b.ano - a.ano;
+    }
+
+    return 0
+  });
+
   return (
     <>
+      <div className="filtros_container">
+        <button className="botao_filtro" onClick={()=>setOrdenacao("titulo-asc")}>Título A-Z</button>
+        <button className="botao_filtro" onClick={()=>setOrdenacao("titulo-desc")}>Título Z-A</button>
+
+        <button className="botao_filtro" onClick={()=>setOrdenacao("genero-asc")}>Gênero A-Z</button>
+        <button className="botao_filtro" onClick={()=>setOrdenacao("genero-desc")}>Gênero Z-A</button>
+
+        <button className="botao_filtro" onClick={()=>setOrdenacao("ano-asc")}>Ano ↑</button>
+        <button className="botao_filtro" onClick={()=>setOrdenacao("ano-desc")}>Ano ↓</button>
+
+        <button className="botao_filtro" onClick={()=>setOrdenacao("")}>Limpar</button>
+      </div>
       <div className="busca-container">
         <input
           type="text"
@@ -32,7 +71,7 @@ export default function FilmesUsuarioPage() {
           onChange={(e) => setBusca(e.target.value)}
         />
       </div>
-      <FilmeList filmes={filmesFiltrados} />
+      <FilmeList filmes={filmesOrdenados} />
     </>
   );
 }
